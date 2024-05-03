55ad78342641cc6020cbb33d3e29d7fd72bbc29e
akashpkakash <168820816+akashpkakash@users.noreply.github.com>
2024-05-03 15:44:56 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,24 +1,28 @@
-/*
- * Copyright (c) 2024 Your Name
- * SPDX-License-Identifier: Apache-2.0
- */
-
 `default_nettype none
 
-module tt_um_example (
+// just a stub to keep the Tiny Tapeout tools happy
+
+module tt_um_tkmdemo (
+    input  wire       VGND,
+    input  wire       VPWR,
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
     output wire [7:0] uio_out,  // IOs: Output path
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    inout  wire [7:0] ua, // analog pins
     input  wire       ena,      // will go high when the design is enabled
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
-
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+    assign uio_out = 0;
+    assign uio_oe = 0;
+    assign uo_out(0) = ui_in[0] ^ ui_in[1]; 
+    assign uo_out(0) = ui_in[0] & ui_in[1];
+    assign uo_out[7:2] =6'b0;
+    
+    
+    
+    
 
 endmodule
